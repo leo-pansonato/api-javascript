652952b77e9b4377a355bbfc686853b3a3926dc8
Leonardo Capitani Pansonato <leopansonato@gmail.com>
2025-01-26 17:48:33 -0300

    refact: ajustar formatação e espaçamento nas funções de autenticação

--- src/functions/authFunctions.js
+++ src/functions/authFunctions.js
@@ -9,11 +9,11 @@ const sql = require('../database/conexao');
  * @param user_pass A senha que será utilizada para o login.
  * @return Retorna `id_user` e `token` ou menssagem de erro.
  */
-async function handleLogin (user_email, user_pass) {
+async function handleLogin(user_email, user_pass) {
     try {
         // Procura pelo usuário com email e senha
         const response = await sql.oneOrNone(`SELECT id_user, email FROM users WHERE email = $1 AND pass = $2`, [user_email, user_pass]);
-        
+
         // Retorna erro de Usuário não encontrado.
         if (!response) throw new Error(`Email ou Senha incorretos.`);
 
@@ -23,13 +23,13 @@ async function handleLogin (user_email, user_pass) {
         const token = btoa(`${id_user} | ${email} | ${timestamp}`)
 
         // Gera hash seguro do token
-        bcrypt.hash(token, 10, async function(err, hash)  {
+        bcrypt.hash(token, 10, async function (err, hash) {
             // Insere novo registro de Auth ou atualiza o já existente com o hash.
             await sql.none(`INSERT INTO auth (id_user, hash) VALUES ($1, $2) ON CONFLICT (id_user) DO UPDATE SET hash = EXCLUDED.hash, timestamp = CURRENT_TIMESTAMP`, [id_user, hash]);
         });
 
         // Retornar dados relevantes
-        return { 
+        return {
             login: true,
             id_user,
             token
@@ -55,7 +55,7 @@ async function handleSignin(user_name, user_email, user_tel, user_pass) {
     try {
         // Procura pelo usuário com email e senha
         const response = await sql.oneOrNone(`SELECT id_user, email FROM users WHERE email = $1`, [user_email]);
-        
+
         // Retorna erro de Usuário não encontrado.
         if (response) throw new Error(`Email já cadastrado.`);
 
@@ -67,14 +67,14 @@ async function handleSignin(user_name, user_email, user_tel, user_pass) {
             SELECT id_user
             FROM users
             WHERE email = $5 AND pass = $6;
-        `,[user_name, user_email, user_tel, user_pass, user_email, user_pass]);
-        
+        `, [user_name, user_email, user_tel, user_pass, user_email, user_pass]);
+
         // Verifica se usuário foi inserido corretamente
-        if(!user) throw new Error(`Erro ao cadastrar usuário, tente novamente mais tarde.`);
+        if (!user) throw new Error(`Erro ao cadastrar usuário, tente novamente mais tarde.`);
 
         // Loga o usuário e retorna os dados relevantes
         return await handleLogin(user_email, user_pass);
-        
+
     } catch (signInError) {
         // Retorna o erro de signIn
         return {
@@ -84,7 +84,7 @@ async function handleSignin(user_name, user_email, user_tel, user_pass) {
     }
 }
 
-async function handleVerifyAuth (id_user, token) {
+async function handleVerifyAuth(id_user, token) {
     try {
         // Procura pelo usuário com email e senha
         const response = await sql.oneOrNone(`SELECT id_user, hash FROM auth WHERE id_user = $1`, [id_user]);
@@ -92,10 +92,10 @@ async function handleVerifyAuth (id_user, token) {
 
         const result = await bcrypt.compare(token, hash);
 
-        return {autorizado: result};
+        return { autorizado: result };
 
     } catch (authVerifyError) {
-        return {autorizado: false};
+        return { autorizado: false };
     }
 }
 
